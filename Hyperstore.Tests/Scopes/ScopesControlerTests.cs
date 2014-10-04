11ca96e5eebba8afb77a9f59ff68e1fcb0f374db
Alain Metge <alain.metge@hotmail.fr>
2014-10-04 14:08:10 +0200

    new scopecontroller

--- Hyperstore.Tests/Scopes/ScopesControlerTests.cs
+++ Hyperstore.Tests/Scopes/ScopesControlerTests.cs
@@ -74,9 +74,47 @@ namespace Hyperstore.Tests.Scopes
             Assert.AreEqual(1, scopes.GetScopes(ScopesSelector.Enabled).Count(), 2);
 
             ((ScopeControler<IDomainModel>)scopes).OnSessionCompleted(2);
+            Assert.AreEqual(0, scopes.GetScopes(ScopesSelector.Enabled).Count(), 2);
+
             ((ScopeControler<IDomainModel>)scopes).OnSessionCompleted(3);
 
 
+        }
+
+        [TestMethod]
+        public void Scopes_loadExtension()
+        {
+            IScopeManager<IDomainModel> scopes = new ScopeControler<IDomainModel>(null);
+
+            var domain = new MockDomainModel("Test");
+            var ext = new MockDomainModel("test", "ext");
+
+            scopes.RegisterScope(domain);
+            scopes.OnSessionCreated(null, 2);
+            scopes.EnableScope(domain);
+
+            scopes.OnSessionCreated(null, 3);
+            Assert.AreEqual(domain, scopes.GetActiveScope("Test", 3));
+            scopes.RegisterScope(ext);
+            Assert.AreEqual(domain, scopes.GetActiveScope("Test", 3));
+            scopes.EnableScope(ext);
+            Assert.AreEqual(domain, scopes.GetActiveScope("Test", 3));
+            scopes.OnSessionCreated(null, 4);
+
+            Assert.AreEqual(ext, scopes.GetActiveScope("Test", 4));
+            Assert.AreEqual(domain, scopes.GetActiveScope("Test", 3));
+
+            Assert.AreEqual(1, scopes.GetScopes(ScopesSelector.Enabled).Count(), 4);
+            Assert.AreEqual(2, scopes.GetScopes(ScopesSelector.Loaded).Count(), 4);
+            scopes.UnloadScope(ext);
+
+            Assert.AreEqual(ext, scopes.GetActiveScope("Test", 4));
+            ((ScopeControler<IDomainModel>)scopes).OnSessionCompleted(4);
+
+            Assert.AreEqual(domain, scopes.GetActiveScope("Test", 3));
+
+            ((ScopeControler<IDomainModel>)scopes).OnSessionCompleted(2);
+            ((ScopeControler<IDomainModel>)scopes).OnSessionCompleted(3);
         }
     }
 }
